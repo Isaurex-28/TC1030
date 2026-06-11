c4f2e2d2c9a607ac66dff648f087d81cd584b067
Isauro Garza <91391400+Isaurex-28@users.noreply.github.com>
2026-06-11 00:28:06 -0600

    Comentarios

--- Cliente.h
+++ Cliente.h
@@ -1,3 +1,11 @@
+/*
+ * Proyecto TC1030 - clase Administrador
+ * Isauro Alejandro Garza Elizondo - A01715088
+ *
+ * Se define la clase Cliente, que hereda de Persona.
+ * Cliente con email de contacto y numero de compras realizadas
+ */
+
 #ifndef CLIENTE_H
 #define CLIENTE_H
 
@@ -10,10 +18,11 @@ using namespace std;
 
 class Cliente : public Persona {
     private:
-        string contacto;
-        int numCompras;
+        string contacto; // E-mail de contacto
+        int numCompras; // Número de compras realizadas en la tienda
 
     public:
+        // Constructor por default
         Cliente():Persona(), contacto(""), numCompras(0) {};
         Cliente(string, string, string);
 
@@ -26,27 +35,66 @@ class Cliente : public Persona {
         stringstream mostrar();
 };
 
+/**
+ * Constructor con parámetros
+ *
+ * Inicializa los atributos de Persona y el contacto del cliente.
+ * El contador de compras inicia en 0.
+ *
+ * @param string nombre del cliente, string id  
+ * @param string con email
+ */
 Cliente::Cliente(string nom, string id, string con):Persona(nom, id),
                 numCompras(0){
     contacto = con;
 }
 
+/** 
+ * setContacto asigna el email al Cliente
+ * @param string email
+ * @return 
+ */
 void Cliente::setContacto(string con){
     contacto = con;
 }
 
+/** 
+ * registrarCompra aumenta el contador cada que un cliente
+ * realiza una compra
+ * @param 
+ * @return 
+ */
 void Cliente::registrarCompra(){
     numCompras++;
 }
 
+/** 
+ * getContacto regresa el e-mail del Cliente
+ * @param 
+ * @return string contacto
+ */
 string Cliente::getContacto(){
     return contacto;
 }
 
+/** 
+ * getNumCompras regresa la cantidad de compras realizadas
+ * por el cliente
+ * @param 
+ * @return int número de compras
+ */
 int Cliente::getNumCompras(){
     return numCompras;
 }
 
+/**
+ * mostrar utiliza polimorfismo de la funcion de la clase Persona.
+ * devuelve los datos del cliente como un stringstream,
+ * (nombre, ID, contacto y número de compras).
+ *
+ * @param
+ * @return stringstream con la información del cliente
+ */
 stringstream Cliente::mostrar(){
     stringstream out;
     out << "Nombre: "   << nombre     << endl;
