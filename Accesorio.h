21eda914e0e880fe2b0a2d98980b20a78f268e3e
Isauro Garza <91391400+Isaurex-28@users.noreply.github.com>
2026-06-11 00:12:14 -0600

    Comentarios

--- Accesorio.h
+++ Accesorio.h
@@ -1,3 +1,13 @@
+/*
+ * Proyecto TC1030 - clase Accesorio
+ * Isauro Alejandro Garza Elizondo - A01715088
+ *
+ * Se define la clase Accesorio, que hereda de Producto.
+ * Representa un accesorio o complemento en la tienda, con atributos
+ * específicos como utilidad, color y si se vende en paquete.
+ */
+
+
 #ifndef ACCESORIO_H
 #define ACCESORIO_H
 
@@ -10,11 +20,12 @@ using namespace std;
 
 class Accesorio : public Producto{
     private:
-        string utilidad;
-        string color;
-        bool paquete;
+        string utilidad; // Uso del producto
+        string color; // Color del accesorio
+        bool paquete; // Se vende en paquete?
 
     public: 
+        // Constructor por default
         Accesorio():Producto(),utilidad(""),color(""),paquete(false){}
         Accesorio(string,string,float,int,bool,string,string,bool);
         
@@ -30,6 +41,16 @@ class Accesorio : public Producto{
         stringstream mostrarCliente();
 };
 
+/**
+ * Constructor con parámetros
+ *
+ * Inicializa los atributos de Producto y los específicos de Accesorio.
+ *
+ * @param string nombre del accesorio, string id, float precio
+ * @param int cantidad en inventario, bool disponibilidad, string utilidad 
+ * @param string color, bool true si se vende en paquete
+ * @return
+ */
 Accesorio::Accesorio(string nom, string id, float pri, int can, 
                 bool dis, string uti,string col,bool paq):Producto(nom,id,pri,can,dis){
     utilidad = uti;
@@ -37,31 +58,69 @@ Accesorio::Accesorio(string nom, string id, float pri, int can,
     paquete = paq;
 }
 
+/** 
+ * setUtilidad asigna la utilidad al accesorio
+ * @param string funcion del accesorio
+ * @return 
+ */
 void Accesorio::setUtilidad(string uti){
     utilidad = uti;
 }
 
+/** 
+ * setUtilidad asigna el color al accesorio
+ * @param string color del accesorio
+ * @return 
+ */
 void Accesorio::setColor(string col){
     color = col;
 }
 
+/** 
+ * setUtilidad asigna si el accesorio se vende en paquete
+ * @param bool true si se vende en paquete
+ * @return 
+ */
 void Accesorio::setPaquete(bool paq){
     paquete = paq;
 }
 
-
+/** 
+ * getUtilidad regresa la utilidad al accesorio
+ * @param 
+ * @return string funcion del accesorio
+ */
 string Accesorio::getUtilidad(){
     return utilidad;
 }
 
+/** 
+ * getColor regresa el color del accesorio
+ * @param 
+ * @return string color del accesorio
+ */
 string Accesorio::getColor(){
     return color;
 }
 
+/** 
+ * getPaquete regresa si el accesorio se vende en paquete
+ * @param 
+ * @return bool true si se vende en paquete
+ */
 bool Accesorio::getPaquete(){
     return paquete;
 }
 
+/**
+ * mostrarAdmin utiliza polimorfismo para sobreescribir 
+ + la funcion de la clase abstracta Producto.
+ * Devuelve los datos completos del accesorio para el administrador,
+ * (ID, utilidad, color, precio, cantidad en el inventario y si se vende en paquete).
+ *
+ * @param
+ * @return stringstream con la información del accesorio
+ */
 stringstream Accesorio::mostrarAdmin(){
     stringstream out;
     out << "Nombre: " << nombre << endl;
@@ -77,6 +136,15 @@ stringstream Accesorio::mostrarAdmin(){
     return out;
 }
 
+/**
+ * mostrarCliente utiliza polimorfismo para sobreescribir 
+ + la funcion de la clase abstracta Producto.
+ * Devuelve los datos completos del accesorio para el cliente,
+ * (nombre, utilidad, color, precio, y si se vende en paquete).
+ *
+ * @param
+ * @return stringstream con la información del accesorio
+ */
 stringstream Accesorio::mostrarCliente(){
     stringstream out;
     out << "Nombre: " << nombre << endl;
